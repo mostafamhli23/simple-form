55819b9d6a75bf00a18fa00f24048435dad0fe2c
mostafamhli23 <100767886+mostafamhli23@users.noreply.github.com>
2022-05-07 23:58:42 +0300

    simple-form

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from './custom-validators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public frmSignup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.frmSignup = this.createSignupForm();
+  }
+
+  createSignupForm(): FormGroup {
+    return this.fb.group(
+      {
+        name: [
+          null,
+          Validators.compose([Validators.required])
+        ],
+        email: [
+          null,
+          Validators.compose([Validators.email, Validators.required])
+        ],
+        password: [
+          null,
+          Validators.compose([
+            Validators.required,
+            CustomValidators.patternValidator(/\d/, {
+              hasNumber: true
+            }),
+            CustomValidators.patternValidator(/[A-Z]/, {
+              hasCapitalCase: true
+            }),
+            CustomValidators.patternValidator(/[a-z]/, {
+              hasSmallCase: true
+            }),
+            CustomValidators.patternValidator(
+              /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/,
+              {
+                hasSpecialCharacters: true
+              }
+            ),
+            Validators.minLength(8)
+          ])
+        ],
+        confirmPassword: [null, Validators.compose([Validators.required])]
+      },
+      {
+        validator: CustomValidators.passwordMatchValidator
+      }
+    );
+  }
+
+  submit() {
+    console.log(this.frmSignup.value);
+  }
+}
